e1b54fb02aa150edad7be9721907ae4e7417e9aa
wood <louyingxia1991@gmail.com>
2016-04-09 23:51:38 +0800

    CloudKit
    CloudKit

--- FoodPin practice/FoodPin/DiscoverTableViewController.swift
+++ FoodPin practice/FoodPin/DiscoverTableViewController.swift
@@ -11,6 +11,9 @@ import CloudKit
 
 
 class DiscoverTableViewController: UITableViewController {
+    var imageCatch:NSCache = NSCache()
+    
+    @IBOutlet var spinner:UIActivityIndicatorView!
     
     var restaurants:[CKRecord] = []
     
@@ -20,6 +23,19 @@ class DiscoverTableViewController: UITableViewController {
         
         getRecordsFromCloud()
         
+        spinner.center = view.center
+        spinner.hidesWhenStopped = true
+        view.addSubview(spinner)
+        spinner.startAnimating()
+        
+        
+        //来下拉刷新
+        refreshControl = UIRefreshControl()
+        refreshControl?.backgroundColor = UIColor.whiteColor()
+        refreshControl?.tintColor = UIColor.grayColor()
+        refreshControl?.addTarget(self, action: "getRecordsFromCloud", forControlEvents: UIControlEvents.ValueChanged)
+        
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -28,28 +44,69 @@ class DiscoverTableViewController: UITableViewController {
     }
     
     func getRecordsFromCloud(){
+        restaurants.removeAll()
+        tableView.reloadData()
+        
+        
         let cloudContaner = CKContainer.defaultContainer()
         let publicDatebase = cloudContaner.publicCloudDatabase
         let predicate = NSPredicate(value: true)
-        let query = CKQuery(recordType: "Restuarant", predicate: predicate)
-        publicDatebase.performQuery(query, inZoneWithID: nil,completionHandler:  {
-            (results, error) -> Void in
-            
-            if error != nil{
-                print(error)
-                return
+        let query = CKQuery(recordType: "Restaurant", predicate: predicate)
+//        publicDatebase.performQuery(query, inZoneWithID: nil,completionHandler:  {
+//            (results, error) -> Void in
+//            
+//            if error != nil{
+//                print(error)
+//                return
+//            }
+//            if let resuls = results{
+//                print("Completed the download of Restaurant data")
+//                self.restaurants = resuls
+//                self.tableView.reloadData()
+//            }
+//            
+//        })
+        
+        //上面用的是convenience API，下面用operation
+        
+        
+        let queryOperation = CKQueryOperation(query: query)
+        queryOperation.desiredKeys = ["name"]
+        queryOperation.queuePriority = .VeryHigh
+        queryOperation.resultsLimit = 50
+        queryOperation.recordFetchedBlock = {(record:CKRecord!)-> Void in
+            if let restaurantRecord = record {
+                self.restaurants.append(restaurantRecord)
             }
-            if let resuls = results{
-                self.restaurants = resuls
+            
+        }
+        
+        queryOperation.queryCompletionBlock = {(cursor:CKQueryCursor?,error:NSError?)-> Void in
+            if (error != nil){
+                print("failed to get data from iCloud - \(error!.localizedDescription)")
+                return
+                
+            }
+            
+            print("successfully retrieve the data from iCloud")
+            
+            
+            NSOperationQueue.mainQueue().addOperationWithBlock(){
+                self.spinner.stopAnimating()
                 self.tableView.reloadData()
             }
-            
-        })
+        self.refreshControl?.endRefreshing()
+
+        }
+      
         
-        
+        publicDatebase.addOperation(queryOperation)
         
         
     }
+    
+    
+  
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -75,12 +132,53 @@ class DiscoverTableViewController: UITableViewController {
         // Configure the cell...
         let restaurant = restaurants[indexPath.row]
         cell.textLabel?.text = restaurant.objectForKey("name")as? String
-        if let image = restaurant.objectForKey("image")
-        {
-            let imageAsset = image as! CKAsset
-            cell.imageView?.image = UIImage(data: NSData (contentsOfURL:imageAsset.fileURL)!)
+        cell.imageView?.image = UIImage(named: "photoalbum")
+        //先检查有没有缓存
+        
+        if let imageFileURL = imageCatch.objectForKey(restaurant.recordID) as? NSURL {
+            print("get image from cache")
+            cell.imageView?.image = UIImage(data: NSData(contentsOfURL: imageFileURL)!)
+        }
+        else{
+        
+        
+        //开始获取真实图片
+        let publicDatabase = CKContainer.defaultContainer().publicCloudDatabase
+        let fetchRecordsImageOperation = CKFetchRecordsOperation(recordIDs:[restaurant.recordID])
+        fetchRecordsImageOperation.desiredKeys = ["image"]
+        fetchRecordsImageOperation.queuePriority = .VeryHigh
+        
+        fetchRecordsImageOperation.perRecordCompletionBlock = {
+            (record:CKRecord?, recordID:CKRecordID?,error:NSError?) -> Void in
+            if (error != nil){
+                print("fail")
+                return
+            }
+            if let restaurantRecord = record {
+                NSOperationQueue.mainQueue().addOperationWithBlock(){
+                    if let imageAsset = restaurantRecord.objectForKey("image")as? CKAsset{
+                        cell.imageView?.image = UIImage(data: NSData(contentsOfURL: imageAsset.fileURL)!)
+                        
+                        //加载成功之后放进缓存
+                        self.imageCatch.setObject(imageAsset.fileURL, forKey: restaurant.recordID)
+                    }
+            
+        }
+            }
+            }
+        
+    
+        publicDatabase.addOperation(fetchRecordsImageOperation)
+        
         }
         
+//        
+//        if let image = restaurant.objectForKey("image")
+//        {
+//            let imageAsset = image as! CKAsset
+//            cell.imageView?.image = UIImage(data: NSData (contentsOfURL:imageAsset.fileURL)!)
+//        }
+//        
         
         
         
